af4285744495a44a70e0a8e03c23bd8c9d080f67
Jeremy Cook <jcook61592@gmail.com>
2021-04-20 13:40:37 -0400

    Navbar setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Jeremy Cook',
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Resume', path: '/resume' },
+        { title: 'Contact', path: '/contact' },
+      ],
+      home: {
+        title: 'Placeholder for title',
+        subTitle: 'Placeholder for subTitle',
+        headline: 'Check out projects below!',
+      },
+      about: {
+        title: 'About Me',
+      },
+      resume: {
+        title: 'Placeholder for resume (presentation pending)',
+      },
+      contact: {
+        title: "Let's Talk!",
+      },
+    };
+  }
+
+  render() {
+    return (
+      <Router>
+        <Container className='p-0' fluid={true}>
+          <Navbar className='border-bottom' bg='transparent' expand='lg'>
+            <Navbar.Brand>Jeremy Cook</Navbar.Brand>
+            <Navbar.Toggle className='border-0' aria-controls='navbar-toggle' />
+            <Navbar.Collapse id='navbar-toggle'>
+              <Nav className='ml-auto'>
+                <Link className='nav-link' to='/home'>
+                  Home
+                </Link>
+                <Link className='nav-link' to='/about'>
+                  About
+                </Link>
+                <Link className='nav-link' to='/resume'>
+                  Resume
+                </Link>
+                <Link className='nav-link' to='/contact'>
+                  Contact
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
