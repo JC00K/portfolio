633e6f9d094129afdba3b4a978f70b103293106a
Jeremy Cook <jcook61592@gmail.com>
2021-04-26 17:52:58 -0400

    contact component front end

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Message from '../components/Message';
+import Content from '../components/Content';
 
 class Contact extends React.Component {
   constructor(props) {
@@ -12,8 +16,83 @@ class Contact extends React.Component {
     };
   }
 
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      disabled: true,
+    });
+  };
+
   render() {
-    return <p>Contact Works</p>;
+    return (
+      <div>
+        <Message title={this.props.title} />
+
+        <Content>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group>
+              <Form.Label htmlFor='full-name'>Full Name</Form.Label>
+              <Form.Control
+                id='full-name'
+                name='name'
+                type='text'
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label htmlFor='email'>Email</Form.Label>
+              <Form.Control
+                id='email'
+                name='email'
+                type='email'
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label htmlFor='message'>Message</Form.Label>
+              <Form.Control
+                id='message'
+                name='message'
+                as='textarea'
+                rows='3'
+                value={this.state.message}
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+
+            <Button
+              className='d-inline-block'
+              variant='primary'
+              type='submit'
+              disabled={this.state.disabled}
+            >
+              Send
+            </Button>
+
+            {this.state.emailSent === true && (
+              <p className='d-inline success-msg'>Email Sent!</p>
+            )}
+            {this.state.emailSent === false && (
+              <p className='d-inline error-msg'>Email Not Sent!</p>
+            )}
+          </Form>
+        </Content>
+      </div>
+    );
   }
 }
 
