0fe16993d3d424ed02e0df20d0eb2f364ddc15e2
Jeremy Cook <jcook61592@gmail.com>
2021-04-28 18:10:40 -0400

    main cleanup

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 import Message from '../components/Message';
 import Content from '../components/Content';
 
@@ -61,43 +59,3 @@ class Contact extends React.Component {
 }
 
 export default Contact;
-
-// <Form onSubmit={this.handleSubmit}>
-//   <Form.Group>
-//     <Form.Label htmlFor='full-name'>Full Name</Form.Label>
-//     <Form.Control
-//       id='full-name'
-//       name='name'
-//       type='text'
-//       value={this.state.name}
-//       onChange={this.onNameChange}
-//     />
-//   </Form.Group>
-
-//   <Form.Group>
-//     <Form.Label htmlFor='email'>Email</Form.Label>
-//     <Form.Control
-//       id='email'
-//       name='_reply_to'
-//       type='email'
-//       value={this.state.email}
-//       onChange={this.onEmailChange}
-//     />
-//   </Form.Group>
-
-//   <Form.Group>
-//     <Form.Label htmlFor='message'>Message</Form.Label>
-//     <Form.Control
-//       id='message'
-//       name='message'
-//       as='textarea'
-//       rows='3'
-//       value={this.state.message}
-//       onChange={this.onMsgChange}
-//     />
-//   </Form.Group>
-
-//   <Button className='d-inline-block' variant='primary' type='submit'>
-//     Send
-//   </Button>
-// </Form>
