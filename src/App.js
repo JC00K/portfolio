75e0f9c9a7f1bcf0b3fa992fa747dd5a5caea4de
Jeremy Cook <jcook61592@gmail.com>
2021-04-28 18:24:53 -0400

    deployed bug fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,11 +8,9 @@ function Footer() {
     <footer className='mt-5'>
       <Container fluid={true}>
         <Row className='border-top justify-content-between p-3'>
-          <Col className='p-0' md={3} sm={12}>
-            Jeremy Cook
-          </Col>
+          <Col className='p-0' md={3} sm={12} />
           <Col className='p-0 d-flex justify-content-end' md={3}>
-            This site was made by Jeremy Cook.
+            This site was made by Jeremy Cook
           </Col>
         </Row>
       </Container>
