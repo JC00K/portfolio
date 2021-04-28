4551d1cb0772631b9f39db9d8abd39fb2c0bb303
Jeremy Cook <jcook61592@gmail.com>
2021-04-28 18:08:49 -0400

    deploy with netlify

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -41,55 +41,19 @@ class Contact extends React.Component {
             action='https://www.flexyform.com/f/ea45921afa5a1aa5d3a36f6fdfb6adb4c18095de'
           >
             <p>
-              <label>Fullname</label>
-              <input type='text' name='fullname' />
+              <label htmlFor='full-name'>Full Name</label>
+              <input type='text' name='name' />
             </p>
             <p>
-              <label>Email</label>
+              <label htmlFor='email'>Email</label>
               <input type='email' name='_reply_to' />
+            </p>
+            <p>
+              <label htmlFor='message'>Message</label>
+              <input type='text' name='message' />
             </p>
             <button type='submit'>Send</button>
           </form>
-
-          <Form onSubmit={this.handleSubmit}>
-            <Form.Group>
-              <Form.Label htmlFor='full-name'>Full Name</Form.Label>
-              <Form.Control
-                id='full-name'
-                name='name'
-                type='text'
-                value={this.state.name}
-                onChange={this.onNameChange}
-              />
-            </Form.Group>
-
-            <Form.Group>
-              <Form.Label htmlFor='email'>Email</Form.Label>
-              <Form.Control
-                id='email'
-                name='email'
-                type='email'
-                value={this.state.email}
-                onChange={this.onEmailChange}
-              />
-            </Form.Group>
-
-            <Form.Group>
-              <Form.Label htmlFor='message'>Message</Form.Label>
-              <Form.Control
-                id='message'
-                name='message'
-                as='textarea'
-                rows='3'
-                value={this.state.message}
-                onChange={this.onMsgChange}
-              />
-            </Form.Group>
-
-            <Button className='d-inline-block' variant='primary' type='submit'>
-              Send
-            </Button>
-          </Form>
         </Content>
       </div>
     );
@@ -97,3 +61,43 @@ class Contact extends React.Component {
 }
 
 export default Contact;
+
+// <Form onSubmit={this.handleSubmit}>
+//   <Form.Group>
+//     <Form.Label htmlFor='full-name'>Full Name</Form.Label>
+//     <Form.Control
+//       id='full-name'
+//       name='name'
+//       type='text'
+//       value={this.state.name}
+//       onChange={this.onNameChange}
+//     />
+//   </Form.Group>
+
+//   <Form.Group>
+//     <Form.Label htmlFor='email'>Email</Form.Label>
+//     <Form.Control
+//       id='email'
+//       name='_reply_to'
+//       type='email'
+//       value={this.state.email}
+//       onChange={this.onEmailChange}
+//     />
+//   </Form.Group>
+
+//   <Form.Group>
+//     <Form.Label htmlFor='message'>Message</Form.Label>
+//     <Form.Control
+//       id='message'
+//       name='message'
+//       as='textarea'
+//       rows='3'
+//       value={this.state.message}
+//       onChange={this.onMsgChange}
+//     />
+//   </Form.Group>
+
+//   <Button className='d-inline-block' variant='primary' type='submit'>
+//     Send
+//   </Button>
+// </Form>
