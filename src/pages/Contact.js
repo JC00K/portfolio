3e8f6730fb8af3db1b25b9afe2cdeba9ffac981f
Jeremy Cook <jcook61592@gmail.com>
2021-05-19 18:14:16 -0400

    contact updates

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -43,11 +43,27 @@ class Contact extends React.Component {
           >
             <p>
               <FormLabel htmlFor='full-name'>Name</FormLabel>
-              <TextField type='text' name='name' fullWidth required />
+              <h1>
+                <TextField
+                  type='text'
+                  name='name'
+                  variant='outlined'
+                  halfWidth
+                  required
+                />
+              </h1>
             </p>
             <p>
               <FormLabel htmlFor='email'>Email</FormLabel>
-              <TextField type='email' name='_reply_to' fullWidth required />
+              <h1>
+                <TextField
+                  type='email'
+                  name='_reply_to'
+                  variant='outlined'
+                  halfWidth
+                  required
+                />
+              </h1>
             </p>
             <p>
               <FormLabel htmlFor='message'>Message</FormLabel>
